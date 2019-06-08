175ea4281693bdd443e3f5c26204c34b450600ad
Tim Miller <timmil@cisco.com>
2019-06-08 11:04:24 -0400

    Clean up server.js output for CLI output during demo.

--- pod-demos-on-nxos/container/v1/server.js
+++ pod-demos-on-nxos/container/v1/server.js
@@ -4,7 +4,7 @@ var handleRequest = function(request, response) {
   console.log('Received request for URL: ' + request.url);
   response.writeHead(200);
   response.write('This is version 1 of the container\n\n');
-  response.end('Running on IP ' + request.socket.localAddress + '. URL' + request.url);
+  response.end('Running on IP ' + request.socket.localAddress + '. URL PATH ' + request.url + '\n');
 };
 
 var www = http.createServer(handleRequest);
